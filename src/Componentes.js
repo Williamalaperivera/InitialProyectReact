3a61afec6a7f517f1c4150b997a9b0534002a58a
William <williamjoel273@gmail.com>
2024-05-15 21:01:53 -0500

    new funtion, we created two funtions when React is doing change the information when de user write the commnent into the button

--- src/Componentes.js
+++ src/Componentes.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useState  } from 'react'
 
 export default function Componentes() {
+
+  const [text, setText] = useState()
+  const [upDate, setUpdate] = useState()
+
+const textOnChange = (event) => {setText (event.target.value)
+
+  }
+
+  const buttonOnClick = () => {setUpdate (text)
+  }
+    
   return (
-    <div> My Componente learning React</div>
+    <div>
+        <input type ="text" value={text} onChange={textOnChange}/>
+        <button onClick={buttonOnClick}>Actualizar</button>
+        <p>Texto input: {text}</p>
+        <p>Texto Actualizado: {upDate}</p>
+    </div>
   )
 }
